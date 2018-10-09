fcbd49701ed9fadf2d5d19b60fe127d5ff6e2fa3
Joao Santos <joao@gutstickets.org>
2018-10-09 19:22:14 +0200

    Added dependency link with git+https prefix

--- setup.py
+++ setup.py
@@ -318,6 +318,9 @@ setup(
     ],
     install_requires=install_requires(strict_bounds=True,
                                       conda_format=conda_build),
+    dependency_links = [
+        "git+https://github.com/joaopedroasantos/ccxt.git@1.17.94.3#egg=ccxt&subdirectory=python",
+    ],
     extras_require=extras_requires(conda_format=conda_build),
     **conditional_arguments
 )
